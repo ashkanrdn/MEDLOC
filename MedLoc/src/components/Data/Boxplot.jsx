2cde3cf55d28a90356bb9b1283a666603d333f2c
darts <darts@dArtss-MacBookPro.local>
2020-12-14 15:41:06 -0500

    fixed box plot

--- MedLoc/src/components/Data/Boxplot.jsx
+++ MedLoc/src/components/Data/Boxplot.jsx
@@ -1,66 +1,72 @@
 import React from "react";
 import {
-	VictoryChart,
-	VictoryZoomContainer,
-	VictoryLine,
-	VictoryBrushContainer,
-	VictoryAxis,
-	VictoryTheme,
-	VictoryBoxPlot,
-	VictoryLabel
+  VictoryChart,
+  VictoryZoomContainer,
+  VictoryLine,
+  VictoryBrushContainer,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryBoxPlot,
+  VictoryLabel,
 } from "victory";
 
 class Boxplot extends React.Component {
-	//   constructor() {
-	//     super();
-	//     this.state = {};
-	//   }
-	state = {};
+  //   constructor() {
+  //     super();
+  //     this.state = {};
+  //   }
+  state = {};
 
-	handleZoom (domain) {
-		this.setState({ selectedDomain: domain });
-	}
+  handleZoom(domain) {
+    this.setState({ selectedDomain: domain });
+  }
 
-	handleBrush (domain) {
-		this.setState({ zoomDomain: domain });
-	}
+  handleBrush(domain) {
+    this.setState({ zoomDomain: domain });
+  }
 
-	render () {
-		// console.log(this.data);
-		return (
-			<div>
-				<VictoryChart
-					domainPadding={10}
-					width={500}
-					height={300}
-					theme={VictoryTheme.material}
-					// colorScale={"warm"}
-					containerComponent={
-						<VictoryZoomContainer
-							responsive={false}
-							zoomDimension='x'
-							zoomDomain={this.state.zoomDomain}
-							onZoomDomainChange={this.handleZoom.bind(this)}
-						/>
-					}>
-					<VictoryLabel text='Data Visualization' x={225} y={290} textAnchor='middle' />
-					<VictoryBoxPlot
-						style={{
-							data: {
-								fill: "tomato"
-							}
-						}}
-						// boxWidth={20}
-						data={this.props.data.features.map((f) => {
-							return {
-								x: f.properties.clusters,
-								y: f.properties[this.props.columnName]
-							};
-						})}
-					/>
-				</VictoryChart>
-			</div>
-		);
-	}
+  render() {
+    // console.log(this.data);
+    return (
+      <div>
+        <VictoryChart
+          domainPadding={10}
+          width={500}
+          height={300}
+          theme={VictoryTheme.material}
+          // colorScale={"warm"}
+          containerComponent={
+            <VictoryZoomContainer
+              responsive={false}
+              zoomDimension="x"
+              zoomDomain={this.state.zoomDomain}
+              onZoomDomainChange={this.handleZoom.bind(this)}
+            />
+          }
+        >
+          <VictoryLabel
+            text="Data Visualization"
+            x={225}
+            y={290}
+            textAnchor="middle"
+          />
+          <VictoryBoxPlot
+            // style={{
+            // 	data: {
+            // 		fill: "tomato"
+            // 	}
+            // }}
+            // boxWidth={20}
+            data={this.props.data.features.map((f) => {
+              return {
+                x: f.properties.clusters,
+                y: f.properties[this.props.columnName],
+              };
+            })}
+          />
+        </VictoryChart>
+      </div>
+    );
+  }
 }
 export default Boxplot;
