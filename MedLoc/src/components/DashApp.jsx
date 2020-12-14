996f8f3f17b8ea6eb60632e9e8cbf5122db1b853
darts <darts@dArtss-MacBookPro.local>
2020-12-14 15:30:22 -0500

    fixed the api call isse?

--- MedLoc/src/components/DashApp.jsx
+++ MedLoc/src/components/DashApp.jsx
@@ -31,255 +31,265 @@ import Navbar from "./Interface/Navbar";
 
 // ____________________________________APP____________________________________
 
-function DashApp (){
-	// To Do : useMemo hook for improving performance for the functions that are heavy
-	const stageCanvasRef = useRef(null);
-	const [ divHeight, setDivHeight ] = useState("200px");
-	const [ isSending, setIsSending ] = useState(false);
+function DashApp() {
+  // To Do : useMemo hook for improving performance for the functions that are heavy
+  const stageCanvasRef = useRef(null);
+  const [divHeight, setDivHeight] = useState("200px");
+  const [isSending, setIsSending] = useState(false);
 
-	useEffect(() => {
-		// The 'current' property contains info of the reference:
-		// align, title, ... , width, height, etc.
-		if (stageCanvasRef.current) {
-			setDivHeight(stageCanvasRef.current.offsetHeight);
-			// let width  = stageCanvasRef.current.offsetWidth;
-		}
-	});
+  useEffect(() => {
+    // The 'current' property contains info of the reference:
+    // align, title, ... , width, height, etc.
+    if (stageCanvasRef.current) {
+      setDivHeight(stageCanvasRef.current.offsetHeight);
+      // let width  = stageCanvasRef.current.offsetWidth;
+    }
+  });
 
-	//Fetching data
-	const fetchUrl =
-		"https://raw.githubusercontent.com/AhmadzadehSanaz/Studio-Lab-Healthcare-Ellinger/main/Data%20Pipeline/hexagon_collection_master.geojson";
+  //Fetching data
+  const fetchUrl =
+    "https://raw.githubusercontent.com/AhmadzadehSanaz/Studio-Lab-Healthcare-Ellinger/main/Data%20Pipeline/hexagon_collection_master.geojson";
 
-	const [ history, setHistory ] = useState([]);
-	const [ data, setData ] = useState(null);
+  const [history, setHistory] = useState([]);
+  const [data, setData] = useState(null);
 
-	const handleHistory = (newCluster) => {
-		let historyCopy = [ ...history ];
-		historyCopy.push(newCluster);
-		setHistory(historyCopy);
-	};
+  const handleHistory = (newCluster) => {
+    let historyCopy = [...history];
+    historyCopy.push(newCluster);
+    setHistory(historyCopy);
+  };
 
-	const handleIteration = (index) => {
-		let ClusterData = history[index];
-		let dataCopy = JSON.parse(JSON.stringify(data));
-		dataCopy.features.forEach((f) => {
-			f.properties.clusters = ClusterData.fidClusterMap[f.properties.fid];
-		});
-		setData(dataCopy);
-	};
+  const handleIteration = (index) => {
+    let ClusterData = history[index];
+    let dataCopy = JSON.parse(JSON.stringify(data));
+    dataCopy.features.forEach((f) => {
+      f.properties.clusters = ClusterData.fidClusterMap[f.properties.fid];
+    });
+    setData(dataCopy);
+  };
 
-	async function getData (){
-		axios
-			.get(fetchUrl)
-			.then((res) => {
-				let data = res.data;
-				data.features.forEach((f) => (f.properties.clusters = "0"));
-				setData(data);
-			})
-			.catch((err) => {
-				console.log(err.message);
-			});
-	}
-	useEffect(() => {
-		getData();
-	}, []);
+  async function getData() {
+    axios
+      .get(fetchUrl)
+      .then((res) => {
+        let data = res.data;
+        data.features.forEach((f) => (f.properties.clusters = "0"));
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+  useEffect(() => {
+    getData();
+  }, []);
 
-	// State for getting user selected feature which will be passed to maps
-	const [ userSelected, setUserSelected ] = useState("Preview_Map");
+  // State for getting user selected feature which will be passed to maps
+  const [userSelected, setUserSelected] = useState("Preview_Map");
 
-	// state for maps
-	const [ userClicked, setUserClicked ] = useState(false);
+  // state for maps
+  const [userClicked, setUserClicked] = useState(false);
 
-	// state for toggle between table and viz
-	const [ checkedMain, setCheckedMain ] = React.useState(false);
-	const toggleCheckedMain = () => {
-		setCheckedMain((prev) => !prev);
-	};
+  // state for toggle between table and viz
+  const [checkedMain, setCheckedMain] = React.useState(false);
+  const toggleCheckedMain = () => {
+    setCheckedMain((prev) => !prev);
+  };
 
-	// Sate for getting features user selected for running the model
-	const [ userFeatures, setUserFeatures ] = useState(null);
+  // Sate for getting features user selected for running the model
+  const [userFeatures, setUserFeatures] = useState(null);
 
-	// State for submit
+  // State for submit
 
-	const [ userSubmit, setUserSubmit ] = useState(false);
-	const toggleSubmit = () => {
-		setUserSubmit((prev) => !prev);
-	};
+  const [userSubmit, setUserSubmit] = useState(false);
+  const toggleSubmit = () => {
+    setUserSubmit((prev) => !prev);
+  };
 
-	// Sending POST request to ML API using axios
+  // Sending POST request to ML API using axios
 
-	const handleSubmit = (clusterNum, Features) => {
-		let mlApiUrl = "/get_kmeans_cluster/";
+  const handleSubmit = (clusterNum, Features) => {
+    let mlApiUrl = "/get_kmeans_cluster/";
 
-		// replacing space with _ for sending to serve
-		let featuresToAPI = Features.map((item) => item.replace(/ /g, "_"));
-		let mlRequest = {
-			"selected features": featuresToAPI,
-			"number of clusters": clusterNum
-		};
+    // replacing space with _ for sending to serve
+    let featuresToAPI = Features.map((item) => item.replace(/ /g, "_"));
+    let mlRequest = {
+      "selected features": featuresToAPI,
+      "number of clusters": clusterNum,
+    };
 
-		setIsSending(true);
+    setIsSending(true);
 
-		axios
-			.post(mlApiUrl, JSON.stringify(mlRequest), {
-				withCredentials: true,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					Accept: "application/json",
-					"Content-Type": "application/json"
-				}
-			})
-			.then(function (response){
-				let responseData = response.data;
+    axios
+      .post(mlApiUrl, JSON.stringify(mlRequest), {
+        withCredentials: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        let responseData = response.data;
 
-				let fidClusterMap = {};
-				Object.keys(responseData).forEach((f) => {
-					f = responseData[f];
-					fidClusterMap[f.fid] = f.clusters.toString();
-				});
-				handleHistory({ features: featuresToAPI, fidClusterMap: fidClusterMap });
+        let fidClusterMap = {};
+        Object.keys(responseData).forEach((f) => {
+          f = responseData[f];
+          fidClusterMap[f.fid] = f.clusters.toString();
+        });
+        handleHistory({
+          features: featuresToAPI,
+          fidClusterMap: fidClusterMap,
+        });
 
-				let dataCopy = JSON.parse(JSON.stringify(data));
-				dataCopy.features.forEach((f) => {
-					f.properties.clusters = fidClusterMap[f.properties.fid];
-				});
-				setData(dataCopy);
-				setIsSending(false);
-			})
-			.catch(function (error){
-				console.log(error);
-				setIsSending(false);
-			});
-	};
-	//
-	const [ optClusters, setoptClusters ] = useState(null);
+        let dataCopy = JSON.parse(JSON.stringify(data));
+        dataCopy.features.forEach((f) => {
+          f.properties.clusters = fidClusterMap[f.properties.fid];
+        });
+        setData(dataCopy);
+        setIsSending(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setIsSending(false);
+      });
+  };
+  //
+  const [optClusters, setoptClusters] = useState(null);
 
-	if (userFeatures !== null) {
-		let featuresToOptCluster = userFeatures.map((item) => item.replace(/ /g, "_"));
+  // if (userFeatures !== null) {
+  // 	let featuresToOptCluster = userFeatures.map((item) => item.replace(/ /g, "_"));
 
-		let mlOptClusterNumURL = "/get_kmeans_silouhette_optimun_cluster_number/";
+  // 	let mlOptClusterNumURL = "/get_kmeans_silouhette_optimun_cluster_number/";
 
-		let mlOptNumRequest = {
-			"selected features": featuresToOptCluster
-		};
+  // 	let mlOptNumRequest = {
+  // 		"selected features": featuresToOptCluster
+  // 	};
 
-		// console.log(mlOptNumRequest, "ml");
-		axios
-			.post(mlOptClusterNumURL, JSON.stringify(mlOptNumRequest), {
-				withCredentials: true,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					Accept: "application/json",
-					"Content-Type": "application/json"
-				}
-			})
-			.then(function (response){
-				let responseData = response.data;
-				console.log(responseData, "opt");
-			})
-			.catch(function (error){
-				console.log(error);
-			});
+  // 	// console.log(mlOptNumRequest, "ml");
+  // 	axios
+  // 		.post(mlOptClusterNumURL, JSON.stringify(mlOptNumRequest), {
+  // 			withCredentials: true,
+  // 			headers: {
+  // 				"Access-Control-Allow-Origin": "*",
+  // 				Accept: "application/json",
+  // 				"Content-Type": "application/json"
+  // 			}
+  // 		})
+  // 		.then(function (response){
+  // 			let responseData = response.data;
+  // 			console.log(responseData, "opt");
+  // 		})
+  // 		.catch(function (error){
+  // 			console.log(error);
+  // 		});
 
-		// console.log(featuresToOptCluster, "userml");
-	}
+  // 	// console.log(featuresToOptCluster, "userml");
+  // }
 
-	const handleUpdateCluster = (clusterData) => {};
-	return (
-		<div className='App'>
-			{
-				data !== null ? <div className='containerDash'>
-					{/* ------------------ NavBar ------------------*/}
-					<nav className='navDash generalComp'>
-						<Navbar />
-					</nav>
+  const handleUpdateCluster = (clusterData) => {};
+  return (
+    <div className="App">
+      {data !== null ? (
+        <div className="containerDash">
+          {/* ------------------ NavBar ------------------*/}
+          <nav className="navDash generalComp">
+            <Navbar />
+          </nav>
 
-					{/*  ------------------Map History Browser ------------------  */}
-					<div className='mainDash generalComp'>
-						<Synthesizer history={history} handleIteration={handleIteration} />
-					</div>
+          {/*  ------------------Map History Browser ------------------  */}
+          <div className="mainDash generalComp">
+            <Synthesizer history={history} handleIteration={handleIteration} />
+          </div>
 
-					{/*  ------------------ Data Selector ------------------ */}
+          {/*  ------------------ Data Selector ------------------ */}
 
-					<div className='sidebarDash generalComp' style={{ padding: "5px" }}>
-						<ListSelector
-							dataProps={data}
-							methodProps={setUserSelected}
-							featureProps={setUserFeatures}
-							handleSubmit={handleSubmit}
-							userFeatures={userFeatures}
-						/>
-						{
-							isSending ? <div
-								style={{
-									height: "60px",
-									display: "flex",
-									justifyContent: "center",
-									alignItems: "center"
-								}}>
-								<Loading />
-							</div> :
-							<MLSetup
-								handleHistory={handleHistory}
-								handleSubmit={handleSubmit}
-								userFeatures={userFeatures}
-							/>}
-					</div>
+          <div className="sidebarDash generalComp" style={{ padding: "5px" }}>
+            <ListSelector
+              dataProps={data}
+              methodProps={setUserSelected}
+              featureProps={setUserFeatures}
+              handleSubmit={handleSubmit}
+              userFeatures={userFeatures}
+            />
+            {isSending ? (
+              <div
+                style={{
+                  height: "60px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Loading />
+              </div>
+            ) : (
+              <MLSetup
+                handleHistory={handleHistory}
+                handleSubmit={handleSubmit}
+                userFeatures={userFeatures}
+              />
+            )}
+          </div>
 
-					{/*  ------------------ Map Preview ------------------*/}
-					<div className='content4 generalComp'>
-						<span>
-							<PreviewMap
-								dataProps={data}
-								userSelectedItems={userSelected}
-								userClickedProp={userClicked}
-							/>
-						</span>
-					</div>
-					{/* ------------------ Main Map ------------------*/}
-					<div className='content5 generalComp'>
-						<span>
-							<Mainmap
-								dataProps={data}
-								userSelectedItems={"clusters"}
-								userClickedProp={userClicked}
-							/>
-						</span>
-					</div>
+          {/*  ------------------ Map Preview ------------------*/}
+          <div className="content4 generalComp">
+            <span>
+              <PreviewMap
+                dataProps={data}
+                userSelectedItems={userSelected}
+                userClickedProp={userClicked}
+              />
+            </span>
+          </div>
+          {/* ------------------ Main Map ------------------*/}
+          <div className="content5 generalComp">
+            <span>
+              <Mainmap
+                dataProps={data}
+                userSelectedItems={"clusters"}
+                userClickedProp={userClicked}
+              />
+            </span>
+          </div>
 
-					{/* ------------------ Mix Viz ------------------*/}
-					<div
-						className='content6 generalComp'
-						style={{
-							display: "grid",
+          {/* ------------------ Mix Viz ------------------*/}
+          <div
+            className="content6 generalComp"
+            style={{
+              display: "grid",
 
-							placeItems: "center"
-						}}>
-						<BoxPlot data={data} columnName={userSelected} />
-					</div>
+              placeItems: "center",
+            }}
+          >
+            <BoxPlot data={data} columnName={userSelected} />
+          </div>
 
-					{/* ----------- Data Table /Viz ------------------ */}
-					<div className='content7 generalComp' id='tble' ref={stageCanvasRef}>
-						<WorldTable
-							style={{ padding: "5px" }}
-							dataProps={data}
-							userFeaturesProps={userFeatures}
-							heightProp={divHeight}
-						/>
-					</div>
-				</div> :
-				// Loading
-				<div
-					style={{
-						height: "100vh",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center"
-					}}>
-					<Loading />
-				</div>}
-		</div>
-	);
+          {/* ----------- Data Table /Viz ------------------ */}
+          <div className="content7 generalComp" id="tble" ref={stageCanvasRef}>
+            <WorldTable
+              style={{ padding: "5px" }}
+              dataProps={data}
+              userFeaturesProps={userFeatures}
+              heightProp={divHeight}
+            />
+          </div>
+        </div>
+      ) : (
+        // Loading
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Loading />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default DashApp;
